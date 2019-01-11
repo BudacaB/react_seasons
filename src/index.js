70e20ac4c10a693707b0ee7244240eb303f83d00
BudacaB <budaca.b@gmail.com>
2019-01-11 21:47:50 +0200

    reafactoring data loading to lifecycle methods, alternate state initialization

--- src/index.js
+++ src/index.js
@@ -2,21 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
+    state = { lat: null, errorMEssage: '' };
 
-        // THIS IS THE ONLY TIME we do direct assignment
-        // to this state
-        this.state = { lat: null, errorMessage: '' };
-
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-            position => {
-                // we called setState!!!!
-                this.setState({ lat: position.coords.latitude })
-            },
-            err => {
-                this.setState({ errorMessage: err.message })
-            }
+            position => this.setState({ lat: position.coords.latitude }),
+            err => this.setState({ errorMessage: err.message }) 
         );
     }
 
